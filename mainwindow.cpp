084a7129f7296fd1e484d18b57b4684d250bcabb
Luis David López-Roberts <luisdavid.lopezroberts.luzon@gmail.com>
2019-01-04 00:31:12 +0100

    Primera versión del proyecto

---
+++ mainwindow.cpp
@@ -0,0 +1,335 @@
+/***************************************************************************
+**                                                                        **
+**  QCustomPlot, an easy to use, modern plotting widget for Qt            **
+**  Copyright (C) 2011-2018 Emanuel Eichhammer                            **
+**                                                                        **
+**  This program is free software: you can redistribute it and/or modify  **
+**  it under the terms of the GNU General Public License as published by  **
+**  the Free Software Foundation, either version 3 of the License, or     **
+**  (at your option) any later version.                                   **
+**                                                                        **
+**  This program is distributed in the hope that it will be useful,       **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+**  GNU General Public License for more details.                          **
+**                                                                        **
+**  You should have received a copy of the GNU General Public License     **
+**  along with this program.  If not, see http://www.gnu.org/licenses/.   **
+**                                                                        **
+****************************************************************************
+**           Author: Emanuel Eichhammer                                   **
+**  Website/Contact: http://www.qcustomplot.com/                          **
+**             Date: 25.06.18                                             **
+**          Version: 2.0.1                                                **
+****************************************************************************/
+
+/************************************************************************************************************
+**                                                                                                         **
+**  This is the example code for QCustomPlot.                                                              **
+**                                                                                                         **
+**  It demonstrates basic and some advanced capabilities of the widget. The interesting code is inside     **
+**  the "setup(...)Demo" functions of MainWindow.                                                          **
+**                                                                                                         **
+**  In order to see a demo in action, call the respective "setup(...)Demo" function inside the             **
+**  MainWindow constructor. Alternatively you may call setupDemo(i) where i is the index of the demo       **
+**  you want (for those, see MainWindow constructor comments). All other functions here are merely a       **
+**  way to easily create screenshots of all demos for the website. I.e. a timer is set to successively     **
+**  setup all the demos and make a screenshot of the window area and save it in the ./screenshots          **
+**  directory.                                                                                             **
+**                                                                                                         **
+*************************************************************************************************************/
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QDesktopWidget>
+#include <QScreen>
+#include <QMessageBox>
+#include <QMetaEnum>
+
+#define FREC_MUESTREO 10000000 //1 MHz
+
+MainWindow::MainWindow(QWidget *parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+  setGeometry(400, 250, 700, 720);
+  currentAmplitude = 1;
+  currentFrequency = 1000;
+
+  
+  setupDemo(1);
+
+  
+  // for making screenshots of the current demo or all demos (for website screenshots):
+  //QTimer::singleShot(1500, this, SLOT(allScreenShots()));
+  //QTimer::singleShot(4000, this, SLOT(screenShot()));  
+}
+
+void MainWindow::setupDemo(int demoIndex)
+{
+  switch (demoIndex)
+  {
+    case 0:  setupQuadraticDemo(ui->customPlot); break;
+    case 1:  setupSimpleDemo(ui->customPlot); break;
+
+  }
+  setWindowTitle("QCustomPlot: "+demoName);
+  statusBar()->clearMessage();
+  currentDemoIndex = demoIndex;
+  ui->customPlot->replot();
+}
+
+void MainWindow::setupQuadraticDemo(QCustomPlot *customPlot)
+{
+  demoName = "Quadratic Demo";
+  // generate some data:
+  QVector<double> x(101), y(101); // initialize with entries 0..100
+  for (int i=0; i<101; ++i)
+  {
+    x[i] = i/50.0 - 1; // x goes from -1 to 1
+    y[i] = x[i]*x[i];  // let's plot a quadratic function
+  }
+  // create graph and assign data to it:
+  customPlot->addGraph();
+  customPlot->graph(0)->setData(x, y);
+  // give the axes some labels:
+  customPlot->xAxis->setLabel("x");
+  customPlot->yAxis->setLabel("y");
+  // set axes ranges, so we see all data:
+  customPlot->xAxis->setRange(-1, 1);
+  customPlot->yAxis->setRange(0, 1);
+}
+
+void MainWindow::setupSimpleDemo(QCustomPlot *customPlot)
+{
+  demoName = "Simple Demo";
+  //
+
+  // add two new graphs and set their look:
+  customPlot->addGraph();
+  customPlot->graph(0)->setPen(QPen(Qt::blue)); // line color blue for first graph
+  customPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20))); // first graph will be filled with translucent blue
+  customPlot->addGraph();
+  customPlot->graph(1)->setPen(QPen(Qt::red)); // line color red for second graph
+  // generate some points of data (y0 for first, y1 for second graph):
+  QVector<double> x(10000), y0(10000), y1(10000);
+  for (int i=0; i<10000; ++i)
+  {
+    x[i] = i;
+    //y0[i] = qExp(-i/150.0)*qCos(i/10.0); // exponentially decaying cosine
+    //y0[i] = currentAmplitude*qCos(i/10.0);
+    y0[i] = currentAmplitude*qCos(2*M_PI*currentFrequency*i/FREC_MUESTREO);
+    //y1[i] = qExp(-i/150.0);              // exponential envelope
+  }
+  // configure right and top axis to show ticks but no labels:
+  // (see QCPAxisRect::setupFullAxesBox for a quicker method to do this)
+  customPlot->xAxis2->setVisible(true);
+  customPlot->xAxis2->setTickLabels(false);
+  customPlot->yAxis2->setVisible(true);
+  customPlot->yAxis2->setTickLabels(false);
+  // make left and bottom axes always transfer their ranges to right and top axes:
+  connect(customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->xAxis2, SLOT(setRange(QCPRange)));
+  connect(customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->yAxis2, SLOT(setRange(QCPRange)));
+
+  connect(ui->butApplyAmp, SIGNAL (pressed()), this, SLOT (onBotonAmplitud()));
+  connect(ui->butApplyFreq, SIGNAL (pressed()), this, SLOT (onBotonFrecuencia()));
+  //connect(ui->pushButton_2, SIGNAL (pressed()), this, SLOT (accionBotonProgramar()));
+
+  // pass data points to graphs:
+  customPlot->graph(0)->setData(x, y0);
+  //customPlot->graph(1)->setData(x, y1);
+  // let the ranges scale themselves so graph 0 fits perfectly in the visible area:
+  customPlot->graph(0)->rescaleAxes();
+  // same thing for graph 1, but only enlarge ranges (in case graph 1 is smaller than graph 0):
+  //customPlot->graph(1)->rescaleAxes(true);
+  // Note: we could have also just called customPlot->rescaleAxes(); instead
+  // Allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking:
+  customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+  //customPlot->setGeometry(42, 26, 65, 65);
+}
+
+void MainWindow::realtimeDataSlot()
+{
+  static QTime time(QTime::currentTime());
+  // calculate two new data points:
+  double key = time.elapsed()/1000.0; // time elapsed since start of demo, in seconds
+  static double lastPointKey = 0;
+  if (key-lastPointKey > 0.002) // at most add point every 2 ms
+  {
+    // add data to lines:
+    ui->customPlot->graph(0)->addData(key, qSin(key)+qrand()/(double)RAND_MAX*1*qSin(key/0.3843));
+    ui->customPlot->graph(1)->addData(key, qCos(key)+qrand()/(double)RAND_MAX*0.5*qSin(key/0.4364));
+    // rescale value (vertical) axis to fit the current data:
+    //ui->customPlot->graph(0)->rescaleValueAxis();
+    //ui->customPlot->graph(1)->rescaleValueAxis(true);
+    lastPointKey = key;
+  }
+  // make key axis range scroll with the data (at a constant range size of 8):
+  ui->customPlot->xAxis->setRange(key, 8, Qt::AlignRight);
+  ui->customPlot->replot();
+  
+  // calculate frames per second:
+  static double lastFpsKey;
+  static int frameCount;
+  ++frameCount;
+  if (key-lastFpsKey > 2) // average fps over 2 seconds
+  {
+    ui->statusBar->showMessage(
+          QString("%1 FPS, Total Data points: %2")
+          .arg(frameCount/(key-lastFpsKey), 0, 'f', 0)
+          .arg(ui->customPlot->graph(0)->data()->size()+ui->customPlot->graph(1)->data()->size())
+          , 0);
+    lastFpsKey = key;
+    frameCount = 0;
+  }
+}
+
+void MainWindow::bracketDataSlot()
+{
+  double secs = QCPAxisTickerDateTime::dateTimeToKey(QDateTime::currentDateTime());
+  
+  // update data to make phase move:
+  int n = 500;
+  double phase = secs*5;
+  double k = 3;
+  QVector<double> x(n), y(n);
+  for (int i=0; i<n; ++i)
+  {
+    x[i] = i/(double)(n-1)*34 - 17;
+    y[i] = qExp(-x[i]*x[i]/20.0)*qSin(k*x[i]+phase);
+  }
+  ui->customPlot->graph()->setData(x, y);
+  
+  itemDemoPhaseTracer->setGraphKey((8*M_PI+fmod(M_PI*1.5-phase, 6*M_PI))/k);
+  
+  ui->customPlot->replot();
+  
+  // calculate frames per second:
+  double key = secs;
+  static double lastFpsKey;
+  static int frameCount;
+  ++frameCount;
+  if (key-lastFpsKey > 2) // average fps over 2 seconds
+  {
+    ui->statusBar->showMessage(
+          QString("%1 FPS, Total Data points: %2")
+          .arg(frameCount/(key-lastFpsKey), 0, 'f', 0)
+          .arg(ui->customPlot->graph(0)->data()->size())
+          , 0);
+    lastFpsKey = key;
+    frameCount = 0;
+  }
+}
+
+void MainWindow::setupPlayground(QCustomPlot *customPlot)
+{
+  Q_UNUSED(customPlot)
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
+
+void MainWindow::screenShot()
+{
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+  QPixmap pm = QPixmap::grabWindow(qApp->desktop()->winId(), this->x()+2, this->y()+2, this->frameGeometry().width()-4, this->frameGeometry().height()-4);
+#elif QT_VERSION < QT_VERSION_CHECK(5, 5, 0)
+  QPixmap pm = qApp->primaryScreen()->grabWindow(qApp->desktop()->winId(), this->x()+2, this->y()+2, this->frameGeometry().width()-4, this->frameGeometry().height()-4);
+#else
+  QPixmap pm = qApp->primaryScreen()->grabWindow(qApp->desktop()->winId(), this->x()-7, this->y()-7, this->frameGeometry().width()+14, this->frameGeometry().height()+14);
+#endif
+  QString fileName = demoName.toLower()+".png";
+  fileName.replace(" ", "");
+  pm.save("./screenshots/"+fileName);
+  qApp->quit();
+}
+
+void MainWindow::allScreenShots()
+{
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+  QPixmap pm = QPixmap::grabWindow(qApp->desktop()->winId(), this->x()+2, this->y()+2, this->frameGeometry().width()-4, this->frameGeometry().height()-4);
+#elif QT_VERSION < QT_VERSION_CHECK(5, 5, 0)
+  QPixmap pm = qApp->primaryScreen()->grabWindow(qApp->desktop()->winId(), this->x()+2, this->y()+2, this->frameGeometry().width()-4, this->frameGeometry().height()-4);
+#else
+  QPixmap pm = qApp->primaryScreen()->grabWindow(qApp->desktop()->winId(), this->x()-7, this->y()-7, this->frameGeometry().width()+14, this->frameGeometry().height()+14);
+#endif
+  QString fileName = demoName.toLower()+".png";
+  fileName.replace(" ", "");
+  pm.save("./screenshots/"+fileName);
+  
+  if (currentDemoIndex < 19)
+  {
+    if (dataTimer.isActive())
+      dataTimer.stop();
+    dataTimer.disconnect();
+    delete ui->customPlot;
+    ui->customPlot = new QCustomPlot(ui->centralWidget);
+    ui->verticalLayout->addWidget(ui->customPlot);
+    setupDemo(currentDemoIndex+1);
+    // setup delay for demos that need time to develop proper look:
+    int delay = 250;
+    if (currentDemoIndex == 10) // Next is Realtime data demo
+      delay = 12000;
+    else if (currentDemoIndex == 15) // Next is Item demo
+      delay = 5000;
+    QTimer::singleShot(delay, this, SLOT(allScreenShots()));
+  } else
+  {
+    qApp->quit();
+  }
+}
+
+void MainWindow::onBotonAmplitud()
+{
+    QString text = ui->textEditAmp->toPlainText();
+    currentAmplitude = text.toFloat();
+    setupDemo(1);
+}
+
+
+void MainWindow::onBotonFrecuencia()
+{
+    QString text = ui->textEditFreq->toPlainText();
+    currentFrequency = text.toFloat();
+    setupDemo(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ signal_generator.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2019-01-04T00:14:45
+#
+#-------------------------------------------------
+
+QT       += core gui
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets printsupport
+
+TARGET = signal_generator
+TEMPLATE = app
+
+SOURCES += main.cpp\
+           mainwindow.cpp \
+           qcustomplot.cpp
+
+HEADERS  += mainwindow.h \
+            qcustomplot.h
+
+FORMS    += mainwindow.ui
